c727e5a64d30b24a7e46ac8b731ce3fef47c627c
CWorld <cworld0@qq.com>
2025-02-07 02:38:29 +0800

    fix(plugins): :bug: fix unocss not detect dynamic change classes

--- src/plugins/shiki-transformers.ts
+++ src/plugins/shiki-transformers.ts
@@ -81,8 +81,7 @@ export const addLanguage = (): ShikiTransformer => {
       const span = h(
         'span',
         {
-          class:
-            'language transition-opacity duration-300 absolute top-3 right-0 ps-1 pe-3 text-sm bg-muted text-muted-foreground select-none'
+          class: 'language ps-1 pe-3 text-sm bg-muted text-muted-foreground'
         },
         this.options.lang
       )
@@ -101,8 +100,7 @@ export const addCopyButton = (timeout?: number): ShikiTransformer => {
       const button = h(
         'button',
         {
-          class:
-            'copy transition-opacity duration-300 opacity-0 absolute top-3 right-3 text-muted-foreground p-1 box-content border rounded bg-primary-foreground',
+          class: 'copy text-muted-foreground p-1 box-content border rounded bg-primary-foreground',
           'data-code': this.source,
           onclick: `
           navigator.clipboard.writeText(this.dataset.code);
