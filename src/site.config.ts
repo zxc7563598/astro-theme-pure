82aa74e1167a598bdceb50400907b1f7430895ae
CWorld <cworld0@qq.com>
2025-07-19 02:02:28 +0800

    feat(plugins): :wrench: support change content-properties for rehype-external-links

--- src/site.config.ts
+++ src/site.config.ts
@@ -86,10 +86,16 @@ export const theme: ThemeUserConfig = {
   },
 
   content: {
-    externalLinksContent: ' ↗',
+    /** External links configuration */
+    externalLinks: {
+      content: ' ↗',
+      /** Properties for the external links element */
+      properties: {
+        style: 'user-select:none'
+      }
+    },
     /** Blog page size for pagination (optional) */
     blogPageSize: 8,
-    externalLinkArrow: true, // show external link arrow
     // Currently support weibo, x, bluesky
     share: ['weibo', 'x', 'bluesky']
   }
