cb19a905e377b681951fe32764f57b3a8fdaad53
CWorld <cworld0@qq.com>
2025-07-18 20:46:02 +0800

    perf(package): :heavy_minus_sign: remove rehype-external-links rependency & support it locally

--- packages/pure/index.ts
+++ packages/pure/index.ts
@@ -6,9 +6,9 @@ import type { AstroIntegration, RehypePlugins, RemarkPlugins } from 'astro'
 // Integrations
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import rehypeExternalLinks from 'rehype-external-links'
 import UnoCSS from 'unocss/astro'
 
+import rehypeExternalLinks from './plugins/rehype-external-links'
 import { remarkAddZoomable, remarkReadingTime } from './plugins/remark-plugins'
 import { vitePluginUserConfig } from './plugins/virtual-user-config'
 import { UserConfigSchema, type UserInputConfig } from './types/user-config'
