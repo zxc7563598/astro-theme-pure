00a396cce104fe2a5ea2b3728c8bcf8ef724cb34
CWorld <cworld0@qq.com>
2025-11-05 23:46:09 +0800

    fix(package): :bug: fix quote with safer method

--- src/site.config.ts
+++ src/site.config.ts
@@ -131,14 +131,14 @@ export const integ: IntegrationUserConfig = {
     // Hitokoto
     // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
     // server: 'https://v1.hitokoto.cn/?c=i',
-    // target: `(data: { hitokoto?: string }) => (data.hitokoto || 'Error')`
+    // target: `(data) => (data.hitokoto || 'Error')`
     // Quoteable API (down temporarily)
     // https://github.com/lukePeavey/quotable
     // server: 'https://api.quotable.io/quotes/random?maxLength=60',
     // target: `(data) => data[0].content || 'Error'`
     // DummyJSON
     server: 'https://dummyjson.com/quotes/random',
-    target: `(data: { quote?: string }) => (data.quote.length > 80 ? \`\${data.quote.slice(0, 80)}...\` : data.quote || 'Error')`
+    target: `(data) => (data.quote.length > 80 ? \`\${data.quote.slice(0, 80)}...\` : data.quote || 'Error')`
   },
   // UnoCSS typography
   // See: https://unocss.dev/presets/typography
