d18316c0b5141b03953f400f3ba3d43714aa479a
zxc7563598 <junjie.he.925@gmail.com>
2025-11-09 23:00:45 +0800

    build(config): 修改 image 服务的 entrypoint 为 'sharp'

--- astro.config.ts
+++ astro.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
   image: {
     responsiveStyles: true,
     service: {
-      entrypoint: 'astro/assets/services/noop'
+      entrypoint: 'astro/assets/services/sharp'
     }
   },
 
