ef8f28e6a0a4ef2ba95049b358ddc41635f8256c
zxc7563598 <junjie.he.925@gmail.com>
2025-11-20 11:17:54 +0800

    fix(pm2): 调整 ecosystem.config.cjs 脚本

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,20 +1,18 @@
 module.exports = {
   apps: [
     {
+      user: 'root',
       name: 'astro-blog',
       script: '/root/.bun/bin/bun',
       args: '/opt/astro-blog/dist/server/entry.mjs',
       cwd: '/opt/astro-blog',
       env: {
         NODE_ENV: 'production',
-        GIT_SSH_COMMAND: 'ssh -i /root/.ssh/id_rsa',
-        GIT_SSL_NO_VERIFY: '0',
         HOME: '/root',
         USER: 'root',
-        LOGNAME: 'root',
-        PWD: '/opt/astro-blog',
-        SHELL: '/bin/bash',
-        TERM: 'xterm-256color'
+        LANG: 'C.UTF-8',
+        LC_ALL: 'C.UTF-8',
+        PATH: '/root/.bun/bin:/usr/local/btgo/bin:/www/server/nodejs/v22.20.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
       }
     }
   ]
