8c8388d25bc51890ee1cb106826f948fdf4f9852
zxc7563598 <junjie.he.925@gmail.com>
2025-12-05 09:06:22 +0800

    refactor(config): 主题配置调整

--- src/site.config.ts
+++ src/site.config.ts
@@ -26,7 +26,7 @@ export const theme: ThemeUserConfig = {
   },
   /** Set a logo image to show in the homepage. */
   logo: {
-    src: '/src/assets/avatar.png',
+    src: '/src/assets/avatar.jpg',
     alt: 'Avatar'
   },
 
