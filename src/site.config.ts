1978263808c7f7940ac6ee6b800e51da8eef6f13
zxc7563598 <junjie.he.925@gmail.com>
2025-12-05 09:16:08 +0800

    refactor(page): 一些页面的小细节调整

--- src/site.config.ts
+++ src/site.config.ts
@@ -81,7 +81,7 @@ export const theme: ThemeUserConfig = {
       }
     ],
     /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
-    credits: true,
+    credits: false,
     /** Optional details about the social media accounts for this site. */
     social: { github: 'https://github.com/zxc7563598' }
   },
